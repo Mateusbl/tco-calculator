86593da0d0da0042d29f766c6cbfb3f6bcb116c4
Mateus Barbosa <mateusbarbosa999@gmail.com>
2026-08-14 16:41:28 -0300

    feat: register InmetroAdapter and FuelConsumptionService as Spring beans

--- spring-app/src/main/java/com/mateus/tcocalculator/config/BeanConfig.java
+++ spring-app/src/main/java/com/mateus/tcocalculator/config/BeanConfig.java
@@ -5,17 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
 import com.mateus.tcocalculator.adapter.out.fipe.FipeAdapter;
+import com.mateus.tcocalculator.adapter.out.inmetro.InmetroAdapter;
 import com.mateus.tcocalculator.adapter.out.ipva.StaticIpvaAdapter;
 import com.mateus.tcocalculator.adapter.out.licensing.StaticLicensingAdapter;
 import com.mateus.tcocalculator.adapter.out.persistence.CachedVehiclePriceAdapter;
 import com.mateus.tcocalculator.adapter.out.persistence.VehiclePriceCacheRepository;
 import com.mateus.tcocalculator.application.service.BuildVehicleService;
 import com.mateus.tcocalculator.application.service.CalculateTcoService;
+import com.mateus.tcocalculator.application.service.FuelConsumptionService;
 import com.mateus.tcocalculator.domain.DepreciationCalculator;
 import com.mateus.tcocalculator.domain.FuelCostCalculator;
 import com.mateus.tcocalculator.domain.VehicleFactory;
 import com.mateus.tcocalculator.domain.port.in.BuildVehicleUseCase;
 import com.mateus.tcocalculator.domain.port.in.CalculateTcoUseCase;
+import com.mateus.tcocalculator.domain.port.out.FuelConsumptionPort;
 import com.mateus.tcocalculator.domain.port.out.IpvaRatePort;
 import com.mateus.tcocalculator.domain.port.out.LicensingFeePort;
 import com.mateus.tcocalculator.domain.port.out.VehiclePricePort;
@@ -77,5 +80,15 @@ public class BeanConfig {
         LicensingFeePort licensingFeePort
     ) {
         return new CalculateTcoService(fuelCostCalculator, depreciationCalculator, vehiclePricePort, ipvaRatePort, licensingFeePort);
+    }
+
+    @Bean
+    public InmetroAdapter inmetroAdapter(RestClient restClient){
+        return new InmetroAdapter(restClient);
+    }
+
+    @Bean 
+    public FuelConsumptionPort fuelConsumptionPort(InmetroAdapter inmetroAdapter){
+        return new FuelConsumptionService(inmetroAdapter);
     }
 }
